abbf8b06aae67bd2cbcda1c009c0ccef38d4741b
Marcio Alves Barroso <malvesb@thoughtworks.com>
2017-06-23 17:56:15 -0400

    fixing redux connected components to the unit tests

--- src/index.js
+++ src/index.js
@@ -2,14 +2,14 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import { Provider } from 'react-redux';
 
-import App from './app';
+import ConnectedApp from './app';
 import store from './common/store';
 
 const app = document.getElementById("root");
 
 ReactDom.render(
   <Provider store={store}>
-    <App />
+    <ConnectedApp />
   </Provider>,
   app
 );
